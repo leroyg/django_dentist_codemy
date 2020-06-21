bfafa74d915bfc19d3e3b3bd7b02828cd95b4d96
leroyg <leroy.gardner@gmail.com>
2020-06-21 20:01:27 +0300

    contact page and view made

--- dental/website/views.py
+++ dental/website/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def home(request):
-  return render(request, 'home.html', {})
+    return render(request, 'home.html', {})
+
+
+def contact(request):
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+        return render(request, 'contact.html', {'message_name': message_name})
+    else:
+        # return the page
+        return render(request, 'contact.html', {})
